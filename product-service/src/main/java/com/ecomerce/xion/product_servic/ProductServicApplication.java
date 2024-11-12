d75ff714162a43a6b8ffd1f283abff15793f96a0
nahom <nahom@yahoo.com>
2024-11-11 16:27:02 -0800

    ecom project full stack

---
+++ product-service/src/main/java/com/ecomerce/xion/product_servic/ProductServicApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.xion.product_servic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicApplication.class, args);
+	}
+
+}
---
+++ product-service/src/test/java/com/ecomerce/xion/product_servic/ProductServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.xion.product_servic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
